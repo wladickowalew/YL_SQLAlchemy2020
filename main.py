778ae8505886858a5906c450026ecee7a73bad4b
kowlad <wladickowalew@gmail.com>
2020-03-26 12:27:19 +0300

    add registration

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 from data.users import User
 from data.news import News
+from Forms.registration import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -12,13 +13,37 @@ def index():
     news = session.query(News).filter(News.is_private != True)
     return render_template("index.html", news=news)
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
 
 def main():
 	db_session.global_init("db/blogs.sqlite")
 	#for i in range(346):
 	#	addNews(f"News {i}", f"Content news {i}", i+1, False)
 
-	app.run()
+	app.run(port=8080, host='127.0.0.1')
 
 def addUser(name, about, email):
 	user = User()
